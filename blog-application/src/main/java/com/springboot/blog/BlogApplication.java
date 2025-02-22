c156274c6da66a0b870ec3b7b3ef060ab40545a4
Sajan Kamath V <sajankamath28@gmail.com>
2025-02-22 17:59:14 +0530

    final commit

--- blog-application/src/main/java/com/springboot/blog/BlogApplication.java
+++ blog-application/src/main/java/com/springboot/blog/BlogApplication.java
@@ -1,14 +1,19 @@
 package com.springboot.blog;
 
+import com.springboot.blog.model.Role;
+import com.springboot.blog.repository.RoleRepository;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 @OpenAPIDefinition(
@@ -31,7 +36,7 @@ import org.springframework.context.annotation.Bean;
 				url="https://github.com/Sajank28/BlogApplication"
 		)
 )
-public class BlogApplication {
+public class BlogApplication implements CommandLineRunner {
 
 	@Bean
 	public ModelMapper modelMapper(){
@@ -43,4 +48,18 @@ public class BlogApplication {
 		SpringApplication.run(BlogApplication.class, args);
 	}
 
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+
+		Role userRole = new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+
+	}
 }
